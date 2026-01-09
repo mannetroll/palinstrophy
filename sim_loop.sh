b8cebb033f3de52fec80e20480fc7ecc90a7e80b
Torbjörn Sjögren <torbjorn.sjogren@gmail.com>
2026-01-09 20:00:01 +0100

    IT=200000

--- sim_loop.sh
+++ sim_loop.sh
@@ -22,14 +22,14 @@ CSV="sim_metadata.csv"
 echo "N, K0, Re, CFL, VISC, STEPS, PALIN, SIG, TIME, MINUTES, FPS" > "$CSV"
 
 for K in 2 5 10 15 20 30; do
-  for N in 256 384 512 768 1024 1536 2048 3072 4096; do
+  for N in 256 384 512 768 1024 1536; do
     LOG="output_N${N}_K${K}.log"
 
     RE="$(Re_from_N_K0 "$N" "$K")"
 
     echo "Running N=${N} K=${K} RE=${RE} ..."
     # N K RE STEPS CFL backend UPDATE ITERATIONS
-    PYTHONUNBUFFERED=1 uv run -- turbulence "$N" "$K" "$RE" 1E5 0.1 auto 10 50000 2>&1 \
+    PYTHONUNBUFFERED=1 uv run -- turbulence "$N" "$K" "$RE" 1E5 0.1 auto 10 200000 2>&1 \
       | stdbuf -oL -eL tee -a "$LOG" \
       | awk -v csv="$CSV" '
           $0=="N, K0, Re, CFL, VISC, STEPS, PALIN, SIG, TIME, MINUTES, FPS" { grab=1; next }
