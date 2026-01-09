78741a9fbe23de076f965842c02cb1723d61c231
Torbjörn Sjögren <torbjorn.sjogren@gmail.com>
2026-01-09 23:48:25 +0100

    uv run turbulence

--- CUDA.ps1
+++ CUDA.ps1
@@ -2,4 +2,4 @@ $cudaRoot = Join-Path $PWD "Briefcase\v13.1"
 $env:CUDA_PATH = $cudaRoot
 $env:PATH = (Join-Path $cudaRoot "bin") + ";" + (Join-Path $cudaRoot "bin\x64") + ";" + $env:PATH
 
-uv run python -m turbulence
+uv run turbulence
