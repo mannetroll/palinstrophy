6d6510d0b32c915d67dddd8798a7dab3785093fb
Torbjörn Sjögren <torbjorn.sjogren@gmail.com>
2026-01-09 09:27:44 +0100

    U=1

--- palinstrophy/turbo_main.py
+++ palinstrophy/turbo_main.py
@@ -445,7 +445,7 @@ class MainWindow(QMainWindow):
         # Update selector
         self.update_combo = QComboBox()
         self.update_combo.setToolTip("U: Update intervall")
-        self.update_combo.addItems(["2", "5", "10", "20", "50", "100", "1E3"])
+        self.update_combo.addItems(["1", "2", "5", "10", "20", "50", "100", "1E3"])
         self.update_combo.setCurrentText(update)
 
         self.auto_reset_checkbox = QCheckBox()
