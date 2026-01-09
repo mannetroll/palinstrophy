f0edb00516a98525093f0f669326256327833756
Torbjörn Sjögren <torbjorn.sjogren@gmail.com>
2026-01-10 00:04:50 +0100

    venv

--- CUDA.ps1
+++ CUDA.ps1
@@ -1,5 +1,4 @@
 $rel = "Briefcase\v13.1\bin\nvcc.exe"
-
 $drv = Get-PSDrive -PSProvider FileSystem |
   Where-Object { Test-Path (Join-Path $_.Root $rel) } |
   Select-Object -First 1 -ExpandProperty Name
@@ -8,4 +7,4 @@ $env:CUDA_PATH = "$drv`:\Briefcase\v13.1"
 $env:Path = "$env:CUDA_PATH\bin;$env:Path"
 $env:Path = "$env:CUDA_PATH\bin\x64;$env:Path"
 
-uv run turbulence
+.\.venv\Scripts\python.exe -m palinstrophy.turbo_main
