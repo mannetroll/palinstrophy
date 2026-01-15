cc214eae641f02fb97e08c88c8e3a3f1d9783361
Torbjörn Sjögren <torbjorn.sjogren@gmail.com>
2026-01-15 11:31:45 +0100

    setFixedWidth(120)

--- palinstrophy/turbo_main.py
+++ palinstrophy/turbo_main.py
@@ -425,7 +425,7 @@ class MainWindow(QMainWindow):
         self.re_edit = QLineEdit()
         self.re_edit.setToolTip("Reynolds Number (Re)")
         self.re_edit.setReadOnly(True)
-        self.re_edit.setFixedWidth(100)
+        self.re_edit.setFixedWidth(120)
         self.re_edit.setText(str(self.sim.re))
 
         # K0 selector
