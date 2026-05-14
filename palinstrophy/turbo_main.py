bbd2db56bb971b32a399b3bc02a67e5b210e7b07
Torbjörn Sjögren <torbjorn.sjogren@gmail.com>
2026-05-14 18:15:28 +0200

    #bbb

--- palinstrophy/turbo_main.py
+++ palinstrophy/turbo_main.py
@@ -634,9 +634,9 @@ class MainWindow(QMainWindow):
         self.cfl_combo = QComboBox()
         self.cfl_combo.setToolTip("L: Controlling Δt (CFL)")
         self.cfl_combo.addItems([
-            "0.05", "0.1", "0.15", "0.2", "0.25", "0.3", "0.4", "0.5",
-            "0.6", "0.64", "0.75", "0.85", "0.95", "1.0", "1.2", "1.5",
-            "1.8", "2.0", "2.1",
+            "0.1", "0.2", "0.3", "0.5",
+            "0.7", "0.8", "0.9", "1.0", "1.2", "1.5",
+            "1.8", "2.0", "2.1", "2.5",
         ])
         self.cfl_combo.setCurrentText(str(self.sim.cfl))
 
